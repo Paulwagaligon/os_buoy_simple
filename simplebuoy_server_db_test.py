ba878a449aab18163c427fceec6c6914f60ea6e7
CTHung <poilkj09862@gmail.com>
2022-05-20 11:21:53 +0800

    return random number of data

--- simplebuoy_server_db_test.py
+++ simplebuoy_server_db_test.py
@@ -9,27 +9,37 @@ import logging
 from datetime import datetime
 import numpy as np
 from typing import Optional
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
+import random
 
 logger = logging.getLogger(__name__)
 app = FastAPI()
 id_number = 0
 url = "127.0.0.1"
+return_time_stamp = datetime.now(timezone.utc)
 
 
 @app.get("/raw_data/")
 async def getPamGuardData(time_stamp: Optional[datetime]=None):#存進來的資料
-    global id_number
+    global id_number, return_time_stamp
     fs = 51200
-    id_number += 1
     t = np.array(range(0, fs))/fs
     freq = 1000
     amp = 1
     data = np.sin(2*np.pi*freq*t)*amp
-    return_time_stamp = datetime.now(timezone.utc).isoformat("T", "milliseconds")
-    returnValue = {'id': id_number, 'time_stamp':return_time_stamp, 'fs': fs, 'name':'test_data', 'data':data.tolist()}
-    print(id_number)
-    return [returnValue]
+
+    record = int(random.uniform(1, 4))
+    print("Amount of return values: ", record)
+    if record == 0:
+        return [{'record':record}]
+    else:
+        returnList = [{'record':record}]
+        for i in range(record):
+            return_time_stamp += timedelta(seconds=1)
+            id_number += 1
+            returnValue = {'id': id_number, 'time_stamp':return_time_stamp.isoformat("T", "milliseconds"), 'fs': fs, 'name':'test_data', 'data':data.tolist()}
+            returnList.append(returnValue)
+        return returnList
 
 
 if __name__ == "__main__":
